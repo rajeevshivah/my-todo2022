37e3ed05e293779408d17b9ce2da11d11fd8916c
raajeev.ranjann <raajeev.ranjann@gmail.com>
2021-12-08 15:51:17 +0530

    red line done

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -4,18 +4,31 @@ import "./input.css";
 
 const Input = ({ addGoal }) => {
   const [goal, setGoal] = useState();
+  const [isValid, setisValid] = useState(true);
+  const [enteredValue, setEnteredValue] = useState("");
+  const goalInputChangedHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setisValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    addGoal(goal);
+    if (enteredValue.trim().length === 0) {
+      setisValid(false);
+      return;
+    }
+    addGoal(enteredValue);
   };
   return (
     <form className="form" onSubmit={formSubmitHandler}>
       <div className="fields">
-        <label>Course Goal</label>
+        <label className={`${!isValid ? "label" : ""}`}>Course Goal</label>
         <input
           type="text"
-          className="input"
-          onChange={(event) => setGoal(event.target.value)}
+          className={`${!isValid ? "invalid" : "input"}`}
+          onChange={(event) => goalInputChangedHandler(event)}
         />
         <div className="button-div">
           <Button type="submit" />
